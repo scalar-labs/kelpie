0d62f794815bacf8d3fe7885e632089f4e75b6a1
Yuji Ito <yuji@phact-columba.com>
2020-03-26 11:07:11 +0900

    Add Scalar DB transfer test (#8)

---
+++ verification-db/src/main/java/verification/db/transfer/TransferChecker.java
@@ -0,0 +1,146 @@
+package verification.db.transfer;
+
+import com.scalar.db.api.DistributedStorage;
+import com.scalar.db.api.DistributedTransaction;
+import com.scalar.db.api.DistributedTransactionManager;
+import com.scalar.db.api.Get;
+import com.scalar.db.api.Result;
+import com.scalar.db.api.TransactionState;
+import com.scalar.db.exception.transaction.CoordinatorException;
+import com.scalar.db.exception.transaction.CrudException;
+import com.scalar.db.transaction.consensuscommit.Coordinator;
+import com.scalar.kelpie.config.Config;
+import com.scalar.kelpie.exception.PostProcessException;
+import com.scalar.kelpie.modules.PostProcessor;
+import io.github.resilience4j.retry.Retry;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+
+public class TransferChecker extends PostProcessor {
+  private final DistributedTransactionManager manager;
+
+  public TransferChecker(Config config) {
+    super(config);
+    this.manager = Common.getTransactionManager(config);
+  }
+
+  @Override
+  public void execute() {
+    List<Result> results = readRecordsWithRetry();
+
+    int committed = getNumOfCommittedFromCoordinator(config);
+
+    if (!isConsistent(results, committed)) {
+      throw new PostProcessException("Inconsistency happened!");
+    }
+  }
+
+  public List<Result> readRecordsWithRetry() {
+    System.out.println("reading latest records ...");
+
+    Retry retry = Common.getRetry("readRecords");
+    Supplier<List<Result>> decorated = Retry.decorateSupplier(retry, this::readRecords);
+
+    try {
+      return decorated.get();
+    } catch (Exception e) {
+      throw new RuntimeException("Reading records failed repeatedly", e);
+    }
+  }
+
+  @Override
+  public void close() {}
+
+  private List<Result> readRecords() {
+    int numAccounts = (int) config.getUserLong("test_config", "num_accounts");
+    int numTypes = (int) config.getUserLong("test_config", "num_account_types", Common.NUM_TYPES);
+    List<Result> results = new ArrayList<>();
+
+    boolean isFailed = false;
+    DistributedTransaction transaction = manager.start();
+    for (int i = 0; i < numAccounts; i++) {
+      for (int j = 0; j < numTypes; j++) {
+        Get get = Common.prepareGet(i, j);
+        try {
+          transaction.get(get).ifPresent(r -> results.add(r));
+        } catch (CrudException e) {
+          // continue to read other records
+          isFailed = true;
+        }
+      }
+    }
+
+    if (isFailed) {
+      throw new RuntimeException("at least 1 record couldn't be read");
+    }
+
+    return results;
+  }
+
+  private int getNumOfCommittedFromCoordinator(Config config) {
+    DistributedStorage storage = Common.getStorage(config);
+    Coordinator coordinator = new Coordinator(storage);
+
+    System.out.println("reading coordinator status...");
+
+    Retry retry = Common.getRetry("checkCoordinator");
+    Function<String, Optional<Coordinator.State>> getState =
+        id -> {
+          try {
+            return coordinator.getState(id);
+          } catch (CoordinatorException e) {
+            // convert the exception for Retry
+            throw new RuntimeException("Failed to read the state from the coordinator", e);
+          }
+        };
+    Function<String, Optional<Coordinator.State>> decorated =
+        Retry.decorateFunction(retry, getState);
+
+    JsonObject unknownTransactions = getPreviousState().getJsonObject("unknown_transaction");
+    int committed = 0;
+    for (String txId : unknownTransactions.keySet()) {
+      Optional<Coordinator.State> state = decorated.apply(txId);
+      if (state.isPresent() && state.get().getState().equals(TransactionState.COMMITTED)) {
+        JsonArray ids = unknownTransactions.getJsonArray(txId);
+        System.out.println(
+            "id: "
+                + txId
+                + " from: "
+                + ids.getInt(0)
+                + " to: "
+                + ids.getInt(1)
+                + " succeeded, not failed");
+        committed++;
+      }
+    }
+
+    return committed;
+  }
+
+  private boolean isConsistent(List<Result> results, int committed) {
+    int totalVersion = Common.getActualTotalVersion(results);
+    int totalBalance = Common.getActualTotalBalance(results);
+    int expectedTotalVersion = (getPreviousState().getInt("committed") + committed) * 2;
+    int expectedTotalBalance = Common.getTotalInitialBalance(config);
+
+    System.out.println("total version: " + totalVersion);
+    System.out.println("expected total version: " + expectedTotalVersion);
+    System.out.println("total balance: " + totalBalance);
+    System.out.println("expected total balance: " + expectedTotalBalance);
+
+    if (totalVersion != expectedTotalVersion) {
+      System.out.println("version mismatch !");
+      return false;
+    }
+    if (totalBalance != expectedTotalBalance) {
+      System.out.println("balance mismatch !");
+      return false;
+    }
+    return true;
+  }
+}
---
+++ verification-db/src/main/java/verification/db/transfer/TransferPreparer.java
@@ -0,0 +1,107 @@
+package verification.db.transfer;
+
+import com.scalar.db.api.DistributedTransaction;
+import com.scalar.db.api.DistributedTransactionManager;
+import com.scalar.db.api.Put;
+import com.scalar.kelpie.config.Config;
+import com.scalar.kelpie.modules.PreProcessor;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+public class TransferPreparer extends PreProcessor {
+  private static final long DEFAULT_POPULATION_CONCURRENCY = 32L;
+  private static final int NUM_ACCOUNTS_PER_TX = 100;
+
+  private final DistributedTransactionManager manager;
+
+  public TransferPreparer(Config config) {
+    super(config);
+    this.manager = Common.getTransactionManager(config);
+  }
+
+  @Override
+  public void execute() {
+    System.out.println("insert initial values ... ");
+
+    int concurrency =
+        (int)
+            config.getUserLong(
+                "test_config", "population_concurrency", DEFAULT_POPULATION_CONCURRENCY);
+    ExecutorService es = Executors.newCachedThreadPool();
+    List<CompletableFuture> futures = new ArrayList<>();
+    IntStream.range(0, concurrency)
+        .forEach(
+            i -> {
+              CompletableFuture<Void> future =
+                  CompletableFuture.runAsync(
+                      () -> {
+                        new PopulationRunner(i).run();
+                      },
+                      es);
+              futures.add(future);
+            });
+
+    CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()])).join();
+    System.out.println("all records have been inserted");
+  }
+
+  @Override
+  public void close() {}
+
+  private class PopulationRunner {
+    private final int id;
+
+    public PopulationRunner(int threadId) {
+      this.id = threadId;
+    }
+
+    public void run() {
+      int concurrency =
+          (int)
+              config.getUserLong(
+                  "test_config", "population_concurrency", DEFAULT_POPULATION_CONCURRENCY);
+      int numAccounts = (int) config.getUserLong("test_config", "num_accounts");
+      int numPerThread = (numAccounts + concurrency - 1) / concurrency;
+      int start = numPerThread * id;
+      int end = Math.min(numPerThread * (id + 1), numAccounts);
+      IntStream.range(0, (numPerThread + NUM_ACCOUNTS_PER_TX - 1) / NUM_ACCOUNTS_PER_TX)
+          .forEach(
+              i -> {
+                int startId = start + NUM_ACCOUNTS_PER_TX * i;
+                int endId = Math.min(start + NUM_ACCOUNTS_PER_TX * (i + 1), end);
+                populateWithTx(startId, endId);
+              });
+    }
+
+    private void populateWithTx(int startId, int endId) {
+      int numTypes = (int) config.getUserLong("test_config", "types", Common.NUM_TYPES);
+      int retries = 0;
+      while (true) {
+        if (retries++ > 10) {
+          throw new RuntimeException("population failed repeatedly!");
+        }
+        DistributedTransaction transaction = manager.start();
+        IntStream.range(startId, endId)
+            .forEach(
+                i -> {
+                  IntStream.range(0, numTypes)
+                      .forEach(
+                          j -> {
+                            Put put = Common.preparePut(i, j, Common.INITIAL_BALANCE);
+                            transaction.put(put);
+                          });
+                });
+        try {
+          transaction.commit();
+          break;
+        } catch (Exception e) {
+          // ignored
+        }
+      }
+    }
+  }
+}
