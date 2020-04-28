e65552744bf0fe4d20a3a501645a163715963ff5
yito88 <yuji@phact-columba.com>
2020-04-28 16:57:22 +0900

    add interrupt()

--- kelpie/src/main/java/com/scalar/kelpie/executor/OnetimeInjectionExecutor.java
+++ kelpie/src/main/java/com/scalar/kelpie/executor/OnetimeInjectionExecutor.java
@@ -45,7 +45,7 @@ public class OnetimeInjectionExecutor extends InjectionExecutor {
       try {
         Thread.sleep(1000);
       } catch (InterruptedException e) {
-        // ignored
+        Thread.currentThread().interrupt();
       }
     }
 
